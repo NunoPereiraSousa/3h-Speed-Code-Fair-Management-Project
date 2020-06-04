506491a066f4a4ab5a748c02ce0f51e1996b8332
Nuno Sousa <9180579@esmad.ipp.pt>
2020-06-04 10:53:57 +0100

    Hustle

--- Server/Controller/markets.controller.js
+++ Server/Controller/markets.controller.js
@@ -2,7 +2,6 @@ const bcrypt = require('bcryptjs');
 const con = require("../Database/database")
 const expressSanitizer = require('express-sanitizer');
 
-
 function getMarketByUserID(req, res) {
     let id_user = req.params.id;
 
@@ -11,10 +10,11 @@ function getMarketByUserID(req, res) {
         if (!err) {
             return res.status(200).send(result[0]);
         } else
-            console.log('Error while performing Query.', err);
+            return res.status(400).send({
+                "error": err
+            });
     });
 }
-
 
 function getAllMarket(req, res) {
     con.query("SELECT * FROM tp2_market WHERE deleted = 0", function (err,
@@ -22,29 +22,52 @@ function getAllMarket(req, res) {
         if (!err) {
             return res.status(200).send(result[0]);
         } else
-            console.log('Error while performing Query.', err);
+            return res.status(400).send({
+                "error": err
+            });
     });
 }
 
+function addMarket(req, res) {
+    let market = {
+        place: req.body.place,
+        date: req.body.date,
+        admin: req.body.admin,
+        num_tent: req.body.num_tent,
+        name: req.body.name,
+        photo: req.body.photo,
+        deleted: 0
+    }
+
+    con.query(`INSERT INTO tp2_market SET ?`, market, (queryErr, result) => {
+        if (!queryErr) {
+            return res.status(200).send(result);
+        } else {
+            return res.status(400).send({
+                "error": queryErr
+            });
+        }
+    })
+}
 
 function updateMarket(req, res) {
     let id_market = req.params.id;
     let place = req.body.place;
     let date = req.body.date;
-    let num_tent = req.body.num_tent
-    let name = req.body.name
+    let num_tent = req.body.num_tent;
+    let name = req.body.name;
+    let admin = req.body.admin;
 
-
-    con.query("UPDATE tp2_user SET name = ?,last_name=?,photo=?,location=?,birth=? WHERE id_tp2_market = ?", [name, last_name, place, date, num_tent, id_market], function (err,
+    con.query("UPDATE tp2_market SET name = ?, place = ?, date = ?, num_tent = ?, admin = ? WHERE id_tp2_market = ?", [name, place, date, num_tent, admin, id_market], function (err,
         result) {
         if (!err) {
             res.status(200).send(result);
         } else
-            throw err;
+            return res.status(400).send({
+                "error": err
+            });
     });
 }
-
-
 
 function deleteMarket(req, res) {
     let id_market = req.params.id;
@@ -57,14 +80,11 @@ function deleteMarket(req, res) {
     });
 }
 
-
-
-
 function likeMarket(req, res) {
     let id_market = req.body.id_market
     let id_user = req.body.id_user
     if (!err) {
-        con.query(`INSERT INTO tp2_user (id_tp2_user,id_tp2_market) VALUES ('${id_user}','${id_market}')`, (queryErr, result) => {
+        con.query(`INSERT INTO tp2_market_user (id_tp2_user, id_tp2_market) VALUES ('${id_user}','${id_market}')`, (queryErr, result) => {
             if (!queryErr) {
                 console.log("Like added");
                 return res.status(200).send(result);
@@ -75,20 +95,15 @@ function likeMarket(req, res) {
             }
         })
     } else {
-        console.log(err);
+        return res.status(400).send({
+            "error": err
+        });
     }
 }
 
-
-
-
-// assadahjbdjas
-
-
-
-
 module.exports = {
     getMarketByUserID,
+    addMarket,
     deleteMarket,
     updateMarket,
     getAllMarket,
