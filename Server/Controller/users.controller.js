51743f2e881d8e36e3b2ad45b70c0e786e56183b
Nuno Sousa <9180579@esmad.ipp.pt>
2020-06-04 10:39:44 +0100

    Hustle

--- Server/Controller/users.controller.js
+++ Server/Controller/users.controller.js
@@ -16,14 +16,13 @@ function getUsers(req, res) {
 
 function addUsers(req, res) {
     let name = req.body.name;
-    let last_name = req.body.lastName
-    let email = req.body.email
+    let email = req.body.email;
     let username = req.body.username;
     let password = req.sanitize(req.body.password);
 
     bcrypt.hash(password, 10, (err, hash) => {
         if (!err) {
-            con.query(`INSERT INTO tp2_user (id_tp2_user_type,name, last_name,email, username, password) VALUES ('2', '${name}', '${last_name}','${email}','${username}', '${hash}')`, (queryErr, result) => {
+            con.query(`INSERT INTO tp2_user (id_tp2_user_type, name, email, username, password) VALUES ('2', '${name}', '${email}','${username}', '${hash}')`, (queryErr, result) => {
                 if (!queryErr) {
                     console.log("User inserted");
                     return res.status(200).send(result);
@@ -34,7 +33,9 @@ function addUsers(req, res) {
                 }
             })
         } else {
-            console.log(err);
+            return res.status(400).send({
+                "error": queryErr
+            });
         }
     });
 }
@@ -47,35 +48,39 @@ function getUserByID(req, res) {
         if (!err) {
             return res.status(200).send(result[0]);
         } else
-            console.log('Error while performing Query.', err);
+            return res.status(400).send({
+                "error": queryErr
+            });
     });
 }
 
 function updateUser(req, res) {
     let id_user = req.params.id;
     let name = req.body.name;
-    let last_name = req.body.lastName;
-    let photo = req.body.photo
-    let location = req.body.location
-    let birth = req.body.birth
+    let email = req.body.email;
+    let username = req.body.username;
 
-    con.query("UPDATE tp2_user SET name = ?,last_name=?,photo=?,location=?,birth=? WHERE id_tp2_user = ?", [name, last_name, photo, location, birth, id_user], function (err,
+    con.query("UPDATE tp2_user SET name = ?, email = ?, username = ? WHERE id_tp2_user = ?", [name, email, username, id_user], function (err,
         result) {
         if (!err) {
             res.status(200).send(result);
         } else
-            throw err;
+            return res.status(400).send({
+                "error": err
+            });
     });
 }
 
 function deleteUser(req, res) {
     let id_user = req.params.id;
-    con.query("UPDATE tp2_user SET id_tp2_user_type = 3 WHERE id_tp2_user = ?", id_user, function (err,
+    con.query("UPDATE tp2_user SET deleted = 1 WHERE id_tp2_user = ?", id_user, function (queryErr,
         result) {
-        if (!err) {
+        if (!queryErr) {
             return res.json(result);
         } else
-            throw err;
+            return res.status(400).send({
+                "error": queryErr
+            });
     });
 }
 
