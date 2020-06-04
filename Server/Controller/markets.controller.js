580136213d657cc1850746e79475f499a5c6333a
Tiago De Pina <tiagopina20014@gmail..com>
2020-06-04 11:18:12 +0100

    hustle

--- Server/Controller/markets.controller.js
+++ Server/Controller/markets.controller.js
@@ -83,6 +83,7 @@ function deleteMarket(req, res) {
 function likeMarket(req, res) {
     let id_market = req.body.id_market
     let id_user = req.body.id_user
+    
 
     con.query(`INSERT INTO tp2_market_user (id_tp2_user, id_tp2_market) VALUES ('${id_user}','${id_market}')`, (queryErr, result) => {
         if (!queryErr) {
